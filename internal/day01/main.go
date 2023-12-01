ef8842def0411d1b8f933e97e69124cf643cbc46
Felix Hillingshaeuser <felix@mxcore.de>
2023-12-01 10:56:42 +0100

    comment day01 code

--- internal/day01/main.go
+++ internal/day01/main.go
@@ -6,14 +6,18 @@ import (
 	"strings"
 )
 
+// Numbers is an array of our internal struct Number
+// We need a struct in order to write a function on the array
 type Numbers []Number
 
+// Number is our struct for finding and replacing numbers in the input string
 type Number struct {
 	Identifier  string
 	Replacement string
 }
 
-func (n Numbers) findAny(input string) bool {
+// FindAny number (digit or word) in the input string
+func (n Numbers) FindAny(input string) bool {
 	for _, num := range n {
 		idx := strings.Index(input, num.Identifier)
 		if idx >= 0 {
@@ -23,7 +27,8 @@ func (n Numbers) findAny(input string) bool {
 	return false
 }
 
-func (n Numbers) replace(input string) string {
+// Replace word numbers with digits, Replace digits with itself to simply the code
+func (n Numbers) Replace(input string) string {
 	for _, num := range n {
 		input = strings.Replace(input, num.Identifier, num.Replacement, 1)
 	}
@@ -35,6 +40,7 @@ func RunPart1(input []string) int {
 	sum := 0
 	for _, line := range input {
 		firstNumber := 0
+		// go from left(start) to right and break at the first digit
 		for i := 0; i < len(line); i++ {
 			number, err := strconv.Atoi(line[i : i+1])
 			if err == nil && number > 0 {
@@ -43,6 +49,7 @@ func RunPart1(input []string) int {
 			}
 		}
 		secondNumber := 0
+		// go from right(end) to left and break at the first digit
 		for j := len(line); j > 0; j-- {
 			number, err := strconv.Atoi(line[j-1 : j])
 			if err == nil && number > 0 {
@@ -57,6 +64,7 @@ func RunPart1(input []string) int {
 
 // RunPart2 is for the second star of the day
 func RunPart2(input []string) int {
+	// setup replacement and find map
 	numbers := Numbers{
 		{"1", "1"},
 		{"2", "2"},
@@ -81,13 +89,16 @@ func RunPart2(input []string) int {
 	for _, line := range input {
 		extract := ""
 		firstNumber := 0
+		// go from left(start) to right and stop at the first digit or spelled out digit
 		for i := 0; i <= len(line); i++ {
 			extract = line[0:i]
-			if numbers.findAny(extract) {
+			if numbers.FindAny(extract) {
 				break
 			}
 		}
-		extract = numbers.replace(extract)
+		// extract the substring from the start until the found digit/spelled out digit
+		extract = numbers.Replace(extract)
+		// go from left(start) to right, try to parse the found character as digit and stop at the first found digit
 		for i := 0; i < len(extract); i++ {
 			number, err := strconv.Atoi(extract[i : i+1])
 			if err == nil && number > 0 {
@@ -96,13 +107,16 @@ func RunPart2(input []string) int {
 			}
 		}
 		secondNumber := 0
+		// go from right(end) to left and stop at the first digit or spelled out digit
 		for j := len(line) - 1; j >= 0; j-- {
 			extract = line[j:]
-			if numbers.findAny(extract) {
+			if numbers.FindAny(extract) {
 				break
 			}
 		}
-		extract = numbers.replace(extract)
+		// extract the substring from the end until the found digit/spelled out digit
+		extract = numbers.Replace(extract)
+		// go from right(end) to left, try to parse the found character as digit and stop at the first found digit
 		for j := len(extract) - 1; j >= 0; j-- {
 			number, err := strconv.Atoi(extract[j : j+1])
 			if err == nil && number > 0 {
