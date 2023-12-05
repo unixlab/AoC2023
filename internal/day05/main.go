2ffad2a770deaf0547654d707968bf2addc4ff54
Felix Hillingshaeuser <felix@mxcore.de>
2023-12-05 10:32:29 +0100

    add optimized day05 part1

--- internal/day05/main.go
+++ internal/day05/main.go
@@ -2,162 +2,67 @@
 package day05
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 )
 
-// RunPart1 is for the first star of the day
-func RunPart1(input []string) int {
-	minSeed := -1
-	currentMap := ""
-	var seeds []int
-	var seedToSoil [][]int
-	var soilToFertilizer [][]int
-	var fertilizerToWater [][]int
-	var waterToLight [][]int
-	var lightToTemperature [][]int
-	var temperatureToHumidity [][]int
-	var humidityToLocation [][]int
+func parseInputMutations(input []string) [][][]int {
+	var mutations [][][]int
+	var newMutation [][]int
 	for _, line := range input {
 		if line == "" {
 			continue
 		}
 		_, isNumber := strconv.Atoi(line[0:1])
-		if isNumber != nil {
-			currentMap = strings.Split(line, " ")[0]
-			if currentMap != "seeds:" {
-				continue
+		if isNumber == nil {
+			mutationData := strings.Split(line, " ")
+			var newMut []int
+			for _, v := range mutationData {
+				number, _ := strconv.Atoi(v)
+				newMut = append(newMut, number)
+			}
+			newMutation = append(newMutation, newMut)
+		} else {
+			if len(newMutation) > 0 {
+				mutations = append(mutations, newMutation)
+				newMutation = [][]int{}
 			}
 		}
-		switch currentMap {
-		case "seeds:":
+	}
+	return append(mutations, newMutation)
+}
+
+func parseSeeds(input []string) []int {
+	var seeds []int
+	for _, line := range input {
+		if strings.HasPrefix(line, "seeds: ") {
 			seedLine := strings.Split(line, ":")
 			seedLine = strings.Split(strings.TrimSpace(seedLine[1]), " ")
 			for _, v := range seedLine {
 				number, _ := strconv.Atoi(v)
 				seeds = append(seeds, number)
 			}
-		case "seed-to-soil":
-			soilLine := strings.Split(line, " ")
-			var newMap []int
-			for _, v := range soilLine {
-				number, _ := strconv.Atoi(v)
-				newMap = append(newMap, number)
-			}
-			seedToSoil = append(seedToSoil, newMap)
-		case "soil-to-fertilizer":
-			fertilizer := strings.Split(line, " ")
-			var newMap []int
-			for _, v := range fertilizer {
-				number, _ := strconv.Atoi(v)
-				newMap = append(newMap, number)
-			}
-			soilToFertilizer = append(soilToFertilizer, newMap)
-		case "fertilizer-to-water":
-			waterLine := strings.Split(line, " ")
-			var newMap []int
-			for _, v := range waterLine {
-				number, _ := strconv.Atoi(v)
-				newMap = append(newMap, number)
-			}
-			fertilizerToWater = append(fertilizerToWater, newMap)
-		case "water-to-light":
-			lightLine := strings.Split(line, " ")
-			var newMap []int
-			for _, v := range lightLine {
-				number, _ := strconv.Atoi(v)
-				newMap = append(newMap, number)
-			}
-			waterToLight = append(waterToLight, newMap)
-		case "light-to-temperature":
-			temperatureLine := strings.Split(line, " ")
-			var newMap []int
-			for _, v := range temperatureLine {
-				number, _ := strconv.Atoi(v)
-				newMap = append(newMap, number)
-			}
-			lightToTemperature = append(lightToTemperature, newMap)
-		case "temperature-to-humidity":
-			humidityLine := strings.Split(line, " ")
-			var newMap []int
-			for _, v := range humidityLine {
-				number, _ := strconv.Atoi(v)
-				newMap = append(newMap, number)
-			}
-			temperatureToHumidity = append(temperatureToHumidity, newMap)
-		case "humidity-to-location":
-			locationLine := strings.Split(line, " ")
-			var newMap []int
-			for _, v := range locationLine {
-				number, _ := strconv.Atoi(v)
-				newMap = append(newMap, number)
-			}
-			humidityToLocation = append(humidityToLocation, newMap)
+			continue
 		}
 	}
+	return seeds
+}
+
+// RunPart1 is for the first star of the day
+func RunPart1(input []string) int {
+	minSeed := -1
+	seeds := parseSeeds(input)
+	mutations := parseInputMutations(input)
 	for _, seed := range seeds {
-		for _, soil := range seedToSoil {
-			dest := soil[0]
-			source := soil[1]
-			length := soil[2]
-			if seed >= source && seed <= source+length {
-				seed += dest - source
-				break
-			}
-		}
-		for _, fertilizer := range soilToFertilizer {
-			dest := fertilizer[0]
-			source := fertilizer[1]
-			length := fertilizer[2]
-			if seed >= source && seed <= source+length {
-				seed += dest - source
-				break
-			}
-		}
-		for _, water := range fertilizerToWater {
-			dest := water[0]
-			source := water[1]
-			length := water[2]
-			if seed >= source && seed <= source+length {
-				seed += dest - source
-				break
-			}
-		}
-		for _, light := range waterToLight {
-			dest := light[0]
-			source := light[1]
-			length := light[2]
-			if seed >= source && seed <= source+length {
-				seed += dest - source
-				break
-			}
-		}
-		for _, temperature := range lightToTemperature {
-			dest := temperature[0]
-			source := temperature[1]
-			length := temperature[2]
-			if seed >= source && seed <= source+length {
-				seed += dest - source
-				break
-			}
-		}
-		for _, humidity := range temperatureToHumidity {
-			dest := humidity[0]
-			source := humidity[1]
-			length := humidity[2]
-			if seed >= source && seed <= source+length {
-				seed += dest - source
-				break
-			}
-		}
-		for _, location := range humidityToLocation {
-			dest := location[0]
-			source := location[1]
-			length := location[2]
-			if seed >= source && seed <= source+length {
-				seed += dest - source
-				break
+		for _, mutation := range mutations {
+			for _, mutationData := range mutation {
+				dest := mutationData[0]
+				source := mutationData[1]
+				length := mutationData[2]
+				if seed >= source && seed <= source+length {
+					seed += dest - source
+					break
+				}
 			}
 		}
 		if minSeed == -1 || minSeed > seed {
